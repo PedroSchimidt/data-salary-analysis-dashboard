8b93e2f6383e2e30365cca647322b4589a08caff
PedroSchimidt <pedro.schimidtt@gmail.com>
2026-01-30 11:50:59 -0300

    estrutura inicial teste

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+st.set_page_config(
+    page_title="Salary Analytics",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 Salary Analytics — Carreiras em Dados")
+st.caption("Dashboard para análise de salários na área de dados.")
